06d9995d18851dd611310fb74c6af7f80775ed86
sanyam01 <sanyam1396@gmail.com>
2026-01-03 02:10:17 -0700

    fixed codebase structure and added model classes

---
+++ rag/src/main/java/com/mneme/rag/model/ResumeChunk.java
@@ -0,0 +1,25 @@
+package com.mneme.rag.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "resume_chunks")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResumeChunk {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String fileName;
+
+    @Column(name = "embedding", columnDefinition="vector(768)")
+    private float[] embedding;
+
+}
